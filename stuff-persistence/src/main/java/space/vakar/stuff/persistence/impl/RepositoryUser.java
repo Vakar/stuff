0a4a531d79982eebb162fd2159d5d2cbd131ba9b
Leonid <leonid.vakar@gmail.com>
2019-11-14 20:32:52 +0200

    refactor (stuff-persistence): fix access modifiers

--- stuff-persistence/src/main/java/space/vakar/stuff/persistence/impl/RepositoryUser.java
+++ stuff-persistence/src/main/java/space/vakar/stuff/persistence/impl/RepositoryUser.java
@@ -2,4 +2,4 @@ package space.vakar.stuff.persistence.impl;
 
 import space.vakar.stuff.persistence.model.User;
 
-public class RepositoryUser extends AbstractRepository<User> {}
+class RepositoryUser extends AbstractRepository<User> {}

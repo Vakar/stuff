f5046dbe16282641afd517038dc883926876427d
Leonid <leonid.vakar@gmail.com>
2019-12-06 15:34:49 +0200

    chore (stuff-persistence): add logger for AbstractRepository

--- stuff-persistence/src/main/java/space/vakar/stuff/persistence/impl/AbstractRepository.java
+++ stuff-persistence/src/main/java/space/vakar/stuff/persistence/impl/AbstractRepository.java
@@ -1,12 +1,17 @@
 package space.vakar.stuff.persistence.impl;
 
 import java.util.List;
+import java.util.function.BiConsumer;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.query.Query;
 import org.hibernate.service.ServiceRegistry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import space.vakar.stuff.persistence.api.DomainEntity;
 import space.vakar.stuff.persistence.api.Hql;
 import space.vakar.stuff.persistence.api.Repository;
@@ -15,70 +20,115 @@ import space.vakar.stuff.persistence.model.User;
 
 class AbstractRepository<T extends DomainEntity> implements Repository<T> {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(AbstractRepository.class);
+
   private static final String HIBERNATE_CONFIG = "hibernate.cfg.xml";
 
   private SessionFactory sessionFactory = buildSessionFactory();
 
+  private static SessionFactory buildSessionFactory() {
+    Configuration config = new Configuration();
+    config.configure(HIBERNATE_CONFIG);
+    config.addAnnotatedClass(User.class);
+    config.addAnnotatedClass(Stuff.class);
+    ServiceRegistry serviceRegistry =
+        new StandardServiceRegistryBuilder().applySettings(config.getProperties()).build();
+    return config.buildSessionFactory(serviceRegistry);
+  }
+
   @Override
   public void add(T entity) {
-    getSession().beginTransaction();
-    getSession().save(entity);
-    getSession().getTransaction().commit();
+    entityConsumer.accept(entity, Session::save);
   }
 
   @Override
   public void add(Iterable<T> entities) {
-    Session session = getSession();
-    session.beginTransaction();
-    entities.forEach(session::save);
-    session.getTransaction().commit();
+    entities.forEach(this::add);
   }
 
   @Override
   public void update(T entity) {
-    getSession().beginTransaction();
-    getSession().update(entity);
-    getSession().getTransaction().commit();
+    entityConsumer.accept(entity, Session::update);
   }
 
   @Override
   public void remove(T entity) {
-    getSession().beginTransaction();
-    getSession().delete(entity);
-    getSession().getTransaction().commit();
+    entityConsumer.accept(entity, Session::delete);
   }
 
-  @SuppressWarnings("unchecked")
   @Override
+  @SuppressWarnings("unchecked")
   public void remove(Hql hql) {
-    getSession().beginTransaction();
-    String hqlStr = hql.getHql();
-    Query<T> query = getSession().createQuery(hqlStr);
-    query.executeUpdate();
-    getSession().getTransaction().commit();
+    Transaction transaction = null;
+    Session session = getSession();
+    try {
+      transaction = session.beginTransaction();
+      Query<T> query = session.createQuery(hql.getHql());
+      query.executeUpdate();
+      transaction.commit();
+    } catch (RuntimeException e) {
+      if (transaction != null) {
+        transaction.rollback();
+      }
+      LOGGER.error("Error happens during database transaction with hql: {}", hql.getHql());
+      LOGGER.error(e.getMessage());
+      throw new RepositoryException(e.getMessage(), e);
+    } finally {
+      if (session != null) {
+        session.close();
+      }
+    }
   }
 
-  @SuppressWarnings("unchecked")
   @Override
+  @SuppressWarnings("unchecked")
   public List<T> query(Hql hql) {
-    getSession().beginTransaction();
-    Query<T> query = getSession().createQuery(hql.getHql());
-    List<T> list = query.list();
-    getSession().getTransaction().commit();
+    List<T> list;
+    Transaction t = null;
+    Session session = getSession();
+    try {
+      t = session.beginTransaction();
+      Query<T> query = session.createQuery(hql.getHql());
+      list = query.list();
+      t.commit();
+    } catch (RuntimeException e) {
+      if (t != null) {
+        t.rollback();
+      }
+      LOGGER.error("Error happens during database transaction with hql: {}", hql.getHql());
+      LOGGER.error(e.getMessage());
+      throw new RepositoryException(e.getMessage(), e);
+    } finally {
+      if (session != null) {
+        session.close();
+      }
+    }
     return list;
-  }
-
-  private static SessionFactory buildSessionFactory() {
-    Configuration configObj = new Configuration();
-    configObj.configure(HIBERNATE_CONFIG);
-    configObj.addAnnotatedClass(User.class);
-    configObj.addAnnotatedClass(Stuff.class);
-    ServiceRegistry serviceRegistryObj =
-        new StandardServiceRegistryBuilder().applySettings(configObj.getProperties()).build();
-    return configObj.buildSessionFactory(serviceRegistryObj);
   }
 
   private Session getSession() {
     return sessionFactory.getCurrentSession();
   }
+
+  private BiConsumer<T, BiConsumer<Session, T>> entityConsumer =
+      (entity, operation) -> {
+        Transaction t = null;
+        Session session = getSession();
+        try {
+          t = session.beginTransaction();
+          operation.accept(session, entity);
+          t.commit();
+        } catch (RuntimeException e) {
+          if (t != null) {
+            t.rollback();
+          }
+          LOGGER.error("Error happens during database transaction with entity: {}", entity);
+          LOGGER.error(e.getMessage());
+          throw new RepositoryException(e.getMessage(), e);
+        } finally {
+          if (session != null) {
+            session.close();
+          }
+        }
+      };
 }

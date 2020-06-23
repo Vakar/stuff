39abce2c3a59991eb5d08b739d25952e715a22c9
Leonid <leonid.vakar@gmail.com>
2020-06-23 17:21:21 +0300

    fix (spring-mvc): session timeout

--- stuff-spring-mvc-ui/src/main/java/space/vakar/stuff/springmvc/security/SpringSecurityConfig.java
+++ stuff-spring-mvc-ui/src/main/java/space/vakar/stuff/springmvc/security/SpringSecurityConfig.java
@@ -2,12 +2,18 @@ package space.vakar.stuff.springmvc.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
@@ -17,12 +23,13 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
   @Autowired private UserDetailsService userDetailsService;
 
   @Override
-  protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+  protected void configure(final AuthenticationManagerBuilder auth) {
     auth.authenticationProvider(customAuthProvider);
   }
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
+    CustomAccessDeniedHandler accessDeniedHandler = new CustomAccessDeniedHandler();
     http.authorizeRequests()
         .antMatchers("/stuff/**")
         .access("hasRole('USER')")
@@ -39,6 +46,19 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
         .deleteCookies("JSESSIONID")
         .and()
         .rememberMe()
-        .userDetailsService(userDetailsService);
+        .userDetailsService(userDetailsService)
+        .and()
+        .exceptionHandling()
+        .accessDeniedHandler(accessDeniedHandler);
+  }
+
+  static class CustomAccessDeniedHandler implements AccessDeniedHandler {
+    public void handle(
+        HttpServletRequest req,
+        HttpServletResponse res,
+        AccessDeniedException accessDeniedException)
+        throws IOException {
+      res.sendRedirect(req.getContextPath() + "/");
+    }
   }
 }
